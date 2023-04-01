e1dd8980010845401668a9f4fb9f5d0dd2743207
Evans <ellaboevans@gmail.com>
2023-04-01 01:08:57 +0000

    Refactor Code
    Add Credentials to dotenv file.

--- firebase.js
+++ firebase.js
@@ -1,17 +1,25 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
+import {
+  FIREBASE_AUTH_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_AUTH_PROJECT_ID,
+  FIREBASE_AUTH_STORAGE_BUCKET,
+  FIREBASE_AUTH_MESSAGING_SENDER_ID,
+  FIREBASE_AUTH_APP_ID,
+} from "@env";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB1vo9eLxvNtwEjrYFj70AQc1W8J0LA-GQ",
-  authDomain: "fir-auth-a48d5.firebaseapp.com",
-  projectId: "fir-auth-a48d5",
-  storageBucket: "fir-auth-a48d5.appspot.com",
-  messagingSenderId: "101622503873",
-  appId: "1:101622503873:web:2f5561a5b8df09b739b740",
+  apiKey: FIREBASE_AUTH_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_AUTH_PROJECT_ID,
+  storageBucket: FIREBASE_AUTH_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_AUTH_MESSAGING_SENDER_ID,
+  appId: FIREBASE_AUTH_APP_ID,
 };
 
 // Initialize Firebase
