a8219701da3b60a65a532db9315ee4976d850732
Evans <ellaboevans@gmail.com>
2023-04-05 14:23:09 +0000

    Pass dummy data for ui display

--- components/ItemCategory.js
+++ components/ItemCategory.js
@@ -4,33 +4,50 @@ import PopularCards from "./PopularCards";
 import Axios from "axios";
 
 const ItemCategroy = ({ title }) => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([
+    {
+      id: 1,
+      title: "Ghanaian Dish",
+      image:
+        "https://images.pexels.com/photos/15640312/pexels-photo-15640312.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      rating: 4.1,
+      address: "Accra, Ghana",
+      price: 250.55,
+      stock: false,
+    },
+    {
+      id: 2,
+      title: "Organic Fruits",
+      image:
+        "https://images.pexels.com/photos/13451707/pexels-photo-13451707.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      rating: 4.9,
+      address: "Cape Coast, Ghana",
+      price: 180.98,
+      stock: true,
+    },
+    {
+      id: 3,
+      title: "Cooking Ingredients",
+      image:
+        "https://images.pexels.com/photos/15792418/pexels-photo-15792418.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      rating: 3.9,
+      address: "Winneba, Ghana",
+      price: 250.55,
+      stock: false,
+    },
+    {
+      id: 4,
+      title: "Dinner Dish",
+      image:
+        "https://images.pexels.com/photos/13669533/pexels-photo-13669533.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      rating: 3.2,
+      address: "Tamale, Ghana",
+      price: 180.98,
+      stock: true,
+    },
+  ]);
 
-  useEffect(() => {
-    const options = {
-      method: "GET",
-      url: "https://jsearch.p.rapidapi.com/search",
-      params: {
-        query: "Python developer in Texas, USA",
-        page: "1",
-        num_pages: "1",
-      },
-      headers: {
-        "X-RapidAPI-Key": "065acb88a4mshe9fab85054aff6fp12ec48jsn47201de46c36",
-        "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
-      },
-    };
-
-    Axios.request(options)
-      .then(function (response) {
-        // console.log(response.data);
-        setData(response.data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  }, []);
-
+  useEffect(() => {});
   return (
     <>
       <View className="flex-row items-center mt-8">
@@ -43,17 +60,24 @@ const ItemCategroy = ({ title }) => {
       </View>
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
         <View className="py-4  space-x-2 items-center flex-row ">
-          <PopularCards
+          {data.map((item) => (
+            <PopularCards
+              key={item.id}
+              title={item.title}
+              image={item.image}
+              rating={item.rating}
+              address={item.address}
+              price={item.price}
+              stock={item.stock}
+            />
+          ))}
+          {/* <PopularCards
             image="https://cdn.pixabay.com/photo/2023/03/29/10/00/cherry-blossom-7885057__340.jpg"
             title="Coworking Space"
             rating={4.9}
             address="Newburry Street, Boston"
             price={250.89}
-          />
-          <PopularCards />
-          <PopularCards />
-          <PopularCards />
-          <PopularCards />
+          /> */}
         </View>
       </ScrollView>
     </>
