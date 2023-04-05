c3736959789db7fe8028e5aeb6d0c8b90c1f3497
Evans <ellaboevans@gmail.com>
2023-04-05 15:20:45 +0000

    Add Password Reset Screen

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import OnBoardingScreen from "./screens/OnBoardingScreen";
 import HomeScreen from "./screens/HomeScreen";
+import ForgotPasswordScreen from "./screens/ForgotPasswordScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +15,11 @@ export default function App() {
         <Stack.Screen name="onboarding" component={OnBoardingScreen} />
         <Stack.Screen name="home" component={HomeScreen} />
         <Stack.Screen name="login" component={LoginScreen} />
+        <Stack.Screen
+          name="forgotpassword"
+          component={ForgotPasswordScreen}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name="register" component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
