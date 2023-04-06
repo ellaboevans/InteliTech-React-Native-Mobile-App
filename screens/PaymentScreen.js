7e371f235a1edd41562ae8f5e5a3c05974550fca
Evans <ellaboevans@gmail.com>
2023-04-06 20:02:24 +0000

    [Feature] Build out the Payment Screen

--- screens/PaymentScreen.js
+++ screens/PaymentScreen.js
@@ -6,12 +6,14 @@ import {
   ScrollView,
   Image,
   SafeAreaView,
+  TextInput,
 } from "react-native";
 import React from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import {
   EllipsisVerticalIcon,
   ArrowLeftIcon,
+  CreditCardIcon,
 } from "react-native-heroicons/outline";
 import { MapPinIcon, StarIcon } from "react-native-heroicons/solid";
 
@@ -74,6 +76,117 @@ const PaymentScreen = () => {
                 ${price}
                 <Text className="font-normal text-gray-400">/day</Text>
               </Text>
+            </View>
+          </View>
+          <View className="">
+            <View className="mt-3 flex-row items-center justify-between">
+              <Text className="font-semibold text-[16px] text-[#1D2330] ">
+                Date
+              </Text>
+              <Text className="font-bold text-sm text-[#8b8b8b] underline ">
+                Edit
+              </Text>
+            </View>
+            <View className="py-2">
+              <Text className="text-[#8b8b8b] mb-2">
+                {startDate} - {endDate}
+              </Text>
+              <View>
+                <Text className="font-semibold text-[16px] text-[#1D2330] py-2 ">
+                  Price
+                </Text>
+                <View className="flex-row items-center justify-between">
+                  <Text className="text-[#8b8b8b] mb-2">${price} x 23 day</Text>
+                  <Text className="font-semibold text-[16px] text-[#1D2330]">
+                    $6000
+                  </Text>
+                </View>
+                <View className="flex-row items-center justify-between">
+                  <Text className="text-[#8b8b8b] mb-2">Service fee</Text>
+                  <Text className="font-semibold text-[16px] text-[#1D2330]">
+                    $300
+                  </Text>
+                </View>
+                <View className="border-b-2 border-gray-100 my-2"></View>
+                <View className="flex-row items-center justify-between mb-4">
+                  <Text className="font-semibold text-[16px] text-[#1D2330]">
+                    Total
+                  </Text>
+                  <Text className="font-semibold text-[16px] text-[#1D2330]">
+                    $6300
+                  </Text>
+                </View>
+                <View>
+                  <Text className="font-semibold text-[16px] mb-2">
+                    Payment Method
+                  </Text>
+                  <TouchableOpacity
+                    className="p-2 w-14 mb-2 items-center rounded-md border"
+                    onPress={() =>
+                      alert("Sorry We are yet to integrate payment system")
+                    }
+                  >
+                    <CreditCardIcon size={30} color="#1D2330" opacity={0.8} />
+                  </TouchableOpacity>
+                  <View>
+                    <Text className="text-[#8b8b8b] mb-2 mt-2">
+                      Card Number
+                    </Text>
+                    <View className="py-3 bg-gray-200 rounded-md">
+                      <TextInput
+                        placeholder="0000 0000 0000 0000"
+                        keyboardType="numeric"
+                        className="p-1"
+                      />
+                    </View>
+                    <View className="my-5 flex-row items-center justify-around">
+                      <View>
+                        <Text className="text-[#8b8b8b] w-40 -ml-1 mb-2">
+                          Expire Date
+                        </Text>
+                        <View className="py-3 bg-gray-200  -ml-2 rounded-md">
+                          <TextInput
+                            placeholder="MM/YY"
+                            keyboardType="default"
+                            className="p-1 "
+                          />
+                        </View>
+                      </View>
+                      <View>
+                        <Text className="text-[#8b8b8b] w-40 mb-2">CVV</Text>
+                        <View className="py-3 bg-gray-200 rounded-md">
+                          <TextInput
+                            placeholder="****"
+                            secureTextEntry
+                            keyboardType="numeric"
+                            className="p-1"
+                          />
+                        </View>
+                      </View>
+                    </View>
+                    <View className="flex-row items-center justify-between my-9">
+                      <View>
+                        <Text className="text-[#8b8b8b] text-sm ">Total</Text>
+                        <Text className="font-semibold text-[20px] text-[#1D2330]">
+                          $6300
+                        </Text>
+                      </View>
+                      <TouchableOpacity
+                        className="py-4 bg-[#1D2330] rounded-md"
+                        onPress={() =>
+                          alert(
+                            "We're yet to implement this feature. Thank you."
+                          )
+                        }
+                      >
+                        <Text className="uppercase font-bold text-center text-white text-[20px] w-44 ">
+                          Pay
+                        </Text>
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                </View>
+              </View>
             </View>
           </View>
         </ScrollView>
